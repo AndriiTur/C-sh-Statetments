aed567f92f39734195698b164777a51054df8245
Andrii Tur <andriiturkevych@gmail.com>
2018-09-04 14:03:49 +0300

    added task_3_1

--- C-sh-Statetments/Program.cs
+++ C-sh-Statetments/Program.cs
@@ -10,6 +10,41 @@ namespace C_sh_Statetments
     {
         static void Main(string[] args)
         {
+            #region Task3_1
+            //string firstDigitString;
+            //string secondDigitString;
+            //int firstDigit;
+            //int secondDigit;
+            //int count = 0;
+
+            //Console.Write("Enter first digit: ");
+            //firstDigitString = Console.ReadLine();
+            //bool isDigitF = int.TryParse(firstDigitString, out firstDigit);
+
+            //Console.Write("Enter second digit: ");
+            //secondDigitString = Console.ReadLine();
+            //bool isDigitS = int.TryParse(secondDigitString, out secondDigit);
+
+            //if (isDigitF && isDigitS)
+            //{
+            //    for (int i = firstDigit; i <= secondDigit; i++)
+            //    {
+            //        if (i % 3 == 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("\nYou ente not correct value");
+            //}
+
+            //Console.WriteLine($"\nCount digits from range[{firstDigit},{secondDigit}] divide by 3 = {count}");
+
+            //Console.WriteLine("\nPress any key...");
+            //Console.ReadKey();
+            #endregion
         }
     }
 }
